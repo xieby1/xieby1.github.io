6c13724147a1e2f0fe4cefb606ec03809a9a4500
xieby1 <xieby1@users.noreply.github.com>
2022-04-27 12:23:44 +0000

    deploy: c644786b47cc8ea61f03f269bbcb60a562c21bd1

---
+++ scripts/nix/shell_fhs_x11.nix
@@ -0,0 +1,20 @@
+# https://ryantm.github.io/nixpkgs/builders/special/fhs-environments/
+
+{ pkgs ? import <nixpkgs> {} }:
+
+(pkgs.buildFHSUserEnv {
+  name = "simple-x11-env";
+  targetPkgs = pkgs: (with pkgs;
+    [ udev
+      alsa-lib
+    ]) ++ (with pkgs.xorg;
+    [ libX11
+      libXcursor
+      libXrandr
+    ]);
+  multiPkgs = pkgs: (with pkgs;
+    [ udev
+      alsa-lib
+    ]);
+  runScript = "bash";
+}).env
---
+++ scripts/nix/shell_qemu_tests_tcg.nix
@@ -0,0 +1,17 @@
+# --pure: start a pure reproducible shell
+# 2022.04.24
+# Compile qemu/tests/tcg/x86_64/
+# env CFLAGS=-O make -e build-tcg-tests-x86_64-linux-user
+{ pkgs ? import <nixpkgs> {} }:
+let
+  name = "nix";
+in
+pkgs.mkShell {
+  inherit name;
+  buildInputs = with pkgs; [
+    glibc.static
+  ];
+  inputsFrom = with pkgs; [
+    qemu
+  ];
+}
