7d803f1e7bc51f91804d096bced1e1f1c77e7280
xieby1 <xieby1@outlook.com>
2022-05-13 20:36:13 +0800

    formatxml support drawio

--- src/scripts/bash/formatxml.sh
+++ src/scripts/bash/formatxml.sh
@@ -2,12 +2,22 @@
 
 if [[ $1 == "-h" ]]
 then
-    echo "format xml"
+    echo "${0##*/} <xml_file>"
+    echo "  Format xml file in place."
+    echo "  If the file extension is .drawio,"
+    echo "  this script will try to uncompress it first."
     exit
 fi
 
 IN=$1
 BAK=${1%.*}.bak.${1##*.}
 mv "$IN" "$BAK"
-xmllint --format "$BAK" > "$IN"
+if [[ "${IN##*.}" == "drawio" ]]
+then
+    drawio -u -x -f xml "$BAK" &> /dev/null
+    xmllint --format "${BAK%.*}.xml" > "$IN"
+    rm -f "${BAK%.*}.xml"
+else
+    xmllint --format "$BAK" > "$IN"
+fi
 mv "$BAK" "/tmp/${IN##*/}"
