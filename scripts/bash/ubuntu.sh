88e5bbba31fe6838b93f19c0bf6240b6b6563d78
xieby1 <xieby1@users.noreply.github.com>
2023-04-07 08:53:54 +0000

    deploy: fbaff8ebb04fd784cfa4ae5600719cfc780bac4a

--- scripts/bash/ubuntu.sh
+++ scripts/bash/ubuntu.sh
@@ -1,89 +1,73 @@
-#!/usr/bin/env bash
+#/usr/bin/env bash
 
-# DONE: sudo broken inside container
-#       https://www.redhat.com/sysadmin/sudo-rootless-podman
-#   tldr: not recommend use su/sudo in rootless container
+# parsing arguments
+VERSION=latest
+while [[ ${OPTIND} -le $# ]]; do
+    getopts "lhe" opt
+    case "${opt}" in
+    l|h)
+        echo "aarch64 riscv64 x86_64 18.04 20.04 22.04 22.10 23.04 latest -h -e"
+        exit 0
+    ;;
+    e)
+        ECHO=1
+    ;;
+    *)
+        arg=${!OPTIND}
+        case "$arg" in
+        aarch64|riscv64|x86_64)
+            ARCH=$arg
+        ;;
+        *)
+            VERSION=$arg
+        ;;
+        esac
+        shift
+    ;;
+    esac
+done
+NAME=ubuntu-${ARCH}
 
-VER=20
-
-# create image
-CREATE_IMAGE_FUNC() {
-    mkdir -p /tmp/u${VER}
-    echo "FROM ubuntu:${VER}.04" > /tmp/u${VER}/Dockerfile
-
-    # buildah default not save intermediate layers,
-    # it seems podman layers default true?
-    # ~~one RUN command, avoid redundent layers~~
-    echo "RUN echo 'root:miqbxgyfdA.PY' | chpasswd -e" >> /tmp/u${VER}/Dockerfile
-    echo "RUN useradd -u ${UID} -G users,sudo -p miqbxgyfdA.PY -s /bin/bash xieby1" >> /tmp/u${VER}/Dockerfile
-    echo "RUN apt update" >> /tmp/u${VER}/Dockerfile
-    echo "RUN apt install sudo" >> /tmp/u${VER}/Dockerfile
-
-    sudo podman build -t u${VER} /tmp/u${VER}
-}
-
-# podman volumes always owned by root:
-#   https://github.com/containers/podman/issues/2898
-CREATE_CONTAINER=(
-    "sudo"
-    "podman" "run"
-
-    "-u" "xieby1" # [root container]
+OPTS=(
+    "-u root"
     # "--userns=keep-id" # [rootless container]
+    "--env debian_chroot=$NAME"
+    # "-h" "container-$NAME" # host name
+    "-h localhost" # host name # avoid edit /etc/hosts
+    "--name=$NAME-$VERSION" # container name
 
-    #"-d" # deamon, not exit
-    "--rm" # exit then remove this container
-    "--env" "debian_chroot=u${VER}"
-    # "-h" "container-u${VER}" # host name
-    "-h" "localhost" # host name # avoid edit /etc/hosts
-    "--name='u${VER}'" # container name
-
-    "-v" "/home/xieby1:/home/xieby1"
-    "-v" "/nix:/nix" # make sure symbol links in home work
-
-    "-w" "/home/xieby1" # working directory
+    "-v $HOME:$HOME"
+    "-v /nix:/nix" # make sure symbol links in home work
+    "-v /run/binfmt:/run/binfmt"
+    "-w $PWD" # working directory
 
     # this will cause guest /etc/hosts override by host's
     # use host ip instead, see host's ip address
     "--network=host" # use host network stack
 
     "-it"
-    "u${VER}"
-#    "/usr/bin/tmux"
+    "$NAME:$VERSION"
+    "/bin/bash"
 )
 
-
-OPTIONS=(
-    "-h"
-    "-c"
-)
-
-# Input variables
-if [[ $# -lt 1 ]]
-then
-    eval "${CREATE_CONTAINER[@]}"
-    exit
+# Pull image
+podman images ${NAME}:${VERSION} | grep ${NAME} &> /dev/null
+if [[ $? != 0 ]]; then
+    if [[ $ARCH == "riscv64" ]]; then
+        image_id=$(podman pull --arch ${ARCH} riscv64/ubuntu:$VERSION)
+    else
+        image_id=$(podman pull --arch ${ARCH} ubuntu:$VERSION)
+    fi
+    podman tag $image_id ${NAME}:${VERSION}
 fi
-while [[ ${OPTIND} -le $# ]]
-do
-    getopts "lhc" opt
-    case "${opt}" in
-    l)
-        echo "${OPTIONS[@]}"
-        exit 0;
-    ;;
-    h)
-        echo "ubuntu container"
-        echo "Usage: ${0##*/} [-h] [-c]"
-        echo "  run with no arg: enter a ubuntu container"
-        echo "  -c: reate a ubuntu container image"
-        echo "  -h: show this help"
-        exit 0;
-    ;;
-    c)
-        CREATE_IMAGE_FUNC
-    ;;
-    *)
-        shift
-    esac
-done
+
+# Run container
+if [[ -z $ECHO ]]; then
+    eval "podman run ${OPTS[@]}"
+    podman commit $NAME-$VERSION $NAME:$VERSION
+    podman rm $NAME-$VERSION
+else
+    echo "podman run ${OPTS[@]}"
+    echo podman commit $NAME-$VERSION $NAME:$VERSION
+    echo podman rm $NAME-$VERSION
+fi
